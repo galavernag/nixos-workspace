999c976c9b0f4b862034ae553dffb10b9fde24d8
Guilherme Galaverna <guigalaverna@gmail.com>
2025-11-07 21:41:48 -0300

    feat: import modules

--- flake.nix
+++ flake.nix
@@ -3,5 +3,21 @@
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: { };
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { inherit system; };
+    in {
+      inherit system;
+      nixosConfigurations.nixos-desktop = pkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./modules/core/audio.nix
+          ./modules/core/networking.nix
+          ./modules/core/locale.nix
+          ./modules/core/system.nix
+
+          # Desktop Environments
+          ./modules/desktop/cosmic.nix
+        ];
+      };
+    };
 }
