fcf6f58c94ca31bf979bbd20de430c72501805ff
Guilherme Galaverna <guigalaverna@gmail.com>
2025-11-08 13:11:16 -0300

    feat: remove default comments

--- users/galavernag/home.nix
+++ users/galavernag/home.nix
@@ -6,15 +6,12 @@
 }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+
   home.username = "galavernag";
   home.homeDirectory = lib.mkForce "/home/galavernag";
 
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     neovim
     wget
@@ -27,41 +24,13 @@
     parsec-bin
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/galavernag/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "neovim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
