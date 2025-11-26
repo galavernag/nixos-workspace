f591427059d69af53e07f99b48dcf38fb5917364
Guilherme Galaverna <guigalaverna@gmail.com>
2025-11-26 13:48:10 -0300

    feat: adicionando configurações para suporte da Steam com Proton

---
+++ modules/gaming.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, username, ... }:
+let
+  cfg = config.modules.gaming;
+in {
+  options.modules.gaming.enable = lib.mkEnableOption "Habilita configurações para jogos";
+
+  config = lib.mkIf cfg.enable {
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    programs.steam = {
+      enable = true;
+
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+
+    programs.gamemode.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      mangohud
+      goverlay
+      protonup-qt
+      bottles
+    ];
+
+    boot.kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+  };
+}
