17b13b17981b98219d7755f2b946bf4031b1bd47
Guilherme Galaverna <guigalaverna@gmail.com>
2025-12-10 21:49:09 -0300

    feat: adicionando a senha com hash

--- modules/users.nix
+++ modules/users.nix
@@ -4,6 +4,7 @@
   users.users.galavernag = {
     isNormalUser = true;
     description = "Guilherme Galaverna";
+    hashedPassword = "$y$j9T$3bl3g3hsLzOFeEPCnKhMu0$nBg9jPO7XriJ21q/1OoiLcCCUodcDRm04HrzFTyQUA.";
     extraGroups = [
       "networkmanager"
       "wheel"
